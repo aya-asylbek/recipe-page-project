31e006175be7402978a87d5c653a6ce6a9762143
aya-asylbek <aichurek0990@gmail.com>
2025-01-24 09:59:52 -0800

    Added new listitem Eggplant

---
+++ script.js
@@ -0,0 +1,4 @@
+let newListItem = document.createElement('li');
+newListItem.textContent = 'Eggplant';
+let ingredientsList = document.querySelector('.ingredients ul');
+ingredientsList.appendChild(newListItem);
