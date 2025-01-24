019780aff67e4957d6fa82426773ab03ccd7e1df
aya-asylbek <aichurek0990@gmail.com>
2025-01-24 12:26:49 -0800

    Added print button, checkboxes for ingridients , added youtube links to navbar

--- script.js
+++ script.js
@@ -2,3 +2,37 @@ let newListItem = document.createElement('li');
 newListItem.textContent = 'Eggplant';
 let ingredientsList = document.querySelector('.ingredients ul');
 ingredientsList.appendChild(newListItem);
+
+//const myDiv = document.getElementById("myDiv");
+//let checkbox = document.createElement("input")
+    //checkbox.type = "checkbox";
+    //checkbox.name = "name";
+    //checkbox.value = "value";
+    //checkbox.id = "id";
+    //let label = document.createElement("label");
+//label.htmlFor = "id";
+//label.appendChild(document.createTextNode("beef"));
+//myDiv.appendChild(checkbox);
+//myDiv.appendChild(label);
+
+document.addEventListener('DOMContentLoaded', function () {
+    const ingredientsList = document.querySelector('#ingredientsList'); 
+    const ingredientsItems = ingredientsList.querySelectorAll('li'); 
+
+    ingredientsItems.forEach(function (item, index) {
+
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.id = "ingredient" + index; 
+
+    
+        let label = document.createElement("label");
+        label.htmlFor = checkbox.id;
+        label.appendChild(document.createTextNode(" " + item.textContent));
+
+    
+        item.textContent = '';
+        item.appendChild(checkbox);
+        item.appendChild(label);
+    });
+});
